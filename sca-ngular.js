fe9021e13d084e95d9bf4a9c0b2db71766c7e76d
thiago.gama <tagama@gmail.com>
2015-07-02 14:04:59 -0300

    Acerto gusman

--- sca-ngular.js
+++ sca-ngular.js
@@ -163,7 +163,31 @@ angular.module('sca-ngular', ['http-auth-interceptor', 'base64'])
     .directive('loginPanel', function () {
         return {
             restrict: 'A',
-            template: '<div block-ui block-ui-pattern="/.*\/api\/authentication/"><div class="form-box" id="login-box" ng-controller="loginController" ng-hide="isAuthenticated"><div class="header"><i class="fa fa-lock"></i> Área Restrita</div><form name="loginForm" autocomplete="off"><div class="body bg-gray"><div class="form-group" show-errors><input type="text" name="username" ng-model="username" class="form-control" placeholder="Login" autofocus/><span class="help-block" ng-show="loginForm.username.$error.required">Obrigatório</span></div><div class="form-group" show-errors><input type="password" name="password" ng-model="password" class="form-control" placeholder="Senha"/><span class="help-block" ng-show="loginForm.password.$error.required">Obrigatório</span></div></div><div class="footer bg-gray"><button type="submit" ng-click="submit()" class="btn btn-primary btn-block">Autenticar</button><div class="alert alert-danger alert-dismissable" ng-show="authenticationError"><b>Usuário ou senha inválida!</b></div></div></form></div></div>'
+            template:'<div block-ui block-ui-pattern="/.*\/api\/authentication/">' +
+                            '<div class="form-box" id="login-box" ng-controller="loginController" ng-hide="isAuthenticated">' +
+                                '<div class="header"><i class="fa fa-lock"></i> Área Restrita</div>' +
+                                '<form name="loginForm" autocomplete="off">' +
+                                    '<div class="body bg-gray">' +
+                                        '<div class="floating-label" show-errors="">' +
+                                            '<input type="text" name="username" ng-model="username" class="ng-pristine ng-valid" required="">' +
+                                            '<label>Login</label>' +
+                                            '<strong class="help-block ng-hide" ng-show="loginForm.username.$error.required">Obrigatório</strong>' +
+                                            '<span></span>' +
+                                        '</div>' +
+                                        '<div class="floating-label" show-errors="">' +
+                                            '<input type="password" name="password" ng-model="password" class="ng-pristine ng-valid" required="">' +
+                                            '<label type="text">Senha</label>' +
+                                            '<strong class="help-block ng-hide" ng-show="loginForm.password.$error.required">Obrigatório</strong>' +
+                                            '<span></span>' +
+                                        '</div>' +
+                                    '</div>' +
+                                    '<div class="footer bg-gray">' +
+                                        '<button type="submit" ng-click="submit()" class="btn btn-primary btn-block">Autenticar</button>' +
+                                        '<div class="alert alert-danger alert-dismissable" ng-show="authenticationError"><b>Usuário ou Senha inválida!</b></div>' +
+                                    '</div>' +
+                                '</form>' +
+                            '</div>' +
+                        '</div>'
         }
     })
     .directive('userPanel', function () {
